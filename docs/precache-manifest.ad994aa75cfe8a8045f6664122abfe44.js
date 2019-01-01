ee7f22aec557c89239948b28886d1829808c0b9f
Tan Li Hau <lhtan93@gmail.com>
2019-01-01 12:40:54 +0800

    chore: build website to docs/

---
+++ docs/precache-manifest.ad994aa75cfe8a8045f6664122abfe44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3395325a46785c36cb4e",
+    "url": "/damu/static/js/runtime~main.3395325a.js"
+  },
+  {
+    "revision": "cce4885e09fdc701568e",
+    "url": "/damu/static/js/main.cce4885e.chunk.js"
+  },
+  {
+    "revision": "ab6b066e773464f10bb5",
+    "url": "/damu/static/js/1.ab6b066e.chunk.js"
+  },
+  {
+    "revision": "cce4885e09fdc701568e",
+    "url": "/damu/static/css/main.207a2701.chunk.css"
+  },
+  {
+    "revision": "dadea4a0bdeb4ba7e347ec9a4cc8d003",
+    "url": "/damu/index.html"
+  }
+];
